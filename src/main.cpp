a3753f09ea003cc5e7929a4f89c21c8f6b8b84c9
Dmitriy Beketov <beketov@calltouch.net>
2023-10-20 17:41:11 +0300

    Time library

--- src/main.cpp
+++ src/main.cpp
@@ -3,7 +3,10 @@
 #include "hardware/Storage.h"
 #include <Arduino.h>
 #include <DS1307RTC.h>
-// #include <Streaming.h>
+
+#include <Wire.h>
+#include <TimeLib.h>
+
 
 Display *display;
 Storage *storage;
@@ -54,10 +57,6 @@ void setup()
     pinMode(SPEAKER_PIN, OUTPUT);
     pinMode(HC_SR04_TRIG_PIN, OUTPUT);
     pinMode(HC_SR04_ECHO_PIN, INPUT);
-
-    // Serial.begin(115200);
-    // while (!Serial) {
-    // }
 
     display = new Display(LCD_ADDR, LCD_COLS, LCD_ROWS);
     storage = new Storage();
@@ -179,3 +178,78 @@ void loop()
     }
 }
 
+
+
+// uncomment for set datetime!
+/*
+const char *monthName[12] = {
+  "Jan", "Feb", "Mar", "Apr", "May", "Jun",
+  "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"
+};
+
+tmElements_t tm;
+
+bool getTime(const char *str)
+{
+  int Hour, Min, Sec;
+
+  if (sscanf(str, "%d:%d:%d", &Hour, &Min, &Sec) != 3) return false;
+  tm.Hour = Hour;
+  tm.Minute = Min;
+  tm.Second = Sec;
+  return true;
+}
+
+bool getDate(const char *str)
+{
+  char Month[12];
+  int Day, Year;
+  uint8_t monthIndex;
+
+  if (sscanf(str, "%s %d %d", Month, &Day, &Year) != 3) return false;
+  for (monthIndex = 0; monthIndex < 12; monthIndex++) {
+    if (strcmp(Month, monthName[monthIndex]) == 0) break;
+  }
+  if (monthIndex >= 12) return false;
+  tm.Day = Day;
+  tm.Month = monthIndex + 1;
+  tm.Year = CalendarYrToTm(Year);
+  return true;
+}
+
+
+void setup() {
+  bool parse=false;
+  bool config=false;
+
+  if (getDate(__DATE__) && getTime(__TIME__)) {
+    parse = true;
+    if (RTC.write(tm)) {
+      config = true;
+    }
+  }
+
+  Serial.begin(9600);
+  while (!Serial) ; // wait for Arduino Serial Monitor
+  delay(200);
+  if (parse && config) {
+    Serial.print("DS1307 configured Time=");
+    Serial.print(__TIME__);
+    Serial.print(", Date=");
+    Serial.println(__DATE__);
+  } else if (parse) {
+    Serial.println("DS1307 Communication Error :-{");
+    Serial.println("Please check your circuitry");
+  } else {
+    Serial.print("Could not parse info from the compiler, Time=\"");
+    Serial.print(__TIME__);
+    Serial.print("\", Date=\"");
+    Serial.print(__DATE__);
+    Serial.println("\"");
+  }
+}
+
+void loop() {
+}
+
+*/
